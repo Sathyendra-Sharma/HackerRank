bd22913e11b8ae9ded4a62bd22f964932e92eb14
Sathyendra.Sharma <sathyendra_sharma@hotmail.com>
2017-10-15 17:20:06 +0530

    java string tokens

--- Java/Strings/JavaStringTokens.java
+++ Java/Strings/JavaStringTokens.java
@@ -1,16 +1,20 @@
 import java.io.*;
 import java.util.*;
-public class Solution {
+public class JavaStringTokens {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();       
-        String[]a = s.trim().split("[ !,?._'@]+");
-        ArrayList<String>listOfStrings =new ArrayList<String>(Arrays.asList(a));
-        System.out.println(listOfStrings.size());
-        for(String str:listOfStrings) {
-            System.out.println(str);
-        }    
+        if (!scan.hasNext()){
+            System.out.println(0);
+        } else {
+            String s=scan.nextLine();
+            String[]a = s.trim().split("[ !,?._'@]+");
+            ArrayList<String>listOfStrings =new ArrayList<String>(Arrays.asList(a));
+            System.out.println(listOfStrings.size());
+            for(String str:listOfStrings) {
+                System.out.println(str);
+            }
+        }
         scan.close();
     }
 }
